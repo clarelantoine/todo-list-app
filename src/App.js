967d17b5c987d4a73a910d2dd7b411a4ab695880
Clarel Antoine <clarel.antoine93@gmail.com>
2023-09-22 01:52:17 +0400

    updated dashboard routes (welcome, notes, and settings)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Dashboard from './routes/dashboard/dashboard.component';
-import Home from './routes/home/home.component';
 import MainLayout from './layouts/main-layout/main-layout.component';
+import DashboardLayout from './layouts/dashboard-layout/dashboard-layout.component';
+import Home from './routes/home/home.component';
 import SignIn from './routes/sigin-in/sigin-in.component';
 import SignUp from './routes/sigin-up/sigin-up.component';
+import Welcome from './routes/welcome/welcome.component';
+import Settings from './routes/settings/settings.component';
+import Notes from './routes/notes/notes.component';
 
 const App = () => (
     <Routes>
@@ -13,7 +16,11 @@ const App = () => (
             <Route path="signin" element={<SignIn />} />
             <Route path="signup" element={<SignUp />} />
         </Route>
-        <Route path="dashboard/*" element={<Dashboard />} />
+        <Route path="dashboard" element={<DashboardLayout />}>
+            <Route index element={<Welcome />} />
+            <Route path="notes" element={<Notes />} />
+            <Route path="settings" element={<Settings />} />
+        </Route>
     </Routes>
 );
 
